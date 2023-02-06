4068ab4319a96bc1c6c8afd878541cde3ad5a067
justinjenecke <justinjenecke@gmail.com>
2023-02-06 10:06:16 +0200

    updates db config and adds zip file for project

--- 04-backend-catch-up/03-lesson/config/DatabaseConfig.php
+++ 04-backend-catch-up/03-lesson/config/DatabaseConfig.php
@@ -8,7 +8,7 @@ class DatabaseConfig {
     private $username = "root";
     private $password = "root";
     private $dbName = "car_sales_tutorial";
-    private $port = 3308; // port number only needs to be used if custom port is used on MAMP
+    private $port = 3306; // port number only needs to be used if custom port is used on MAMP
 
     
     public function connect() {
@@ -24,24 +24,4 @@ class DatabaseConfig {
         }
     }
 }
-
-// ====== Function ======
-
-// function connect() {
-
-//     $host = "localhost";
-//     $username = "root";
-//     $password = "root";
-//     $dbName = "car_sales_tutorial";
-//     $port = 3308; // port number only needs to be used if custom port is used on MAMP
-
-
-//     $conn = new mysqli($host, $username, $this->password, $this->dbName, $this->port);
-//     // Check connection
-//     if ($conn->connect_error) {
-//         die("Connection failed: " . $conn->connect_error); //die function to close connection in case of error
-//     } else {
-//         return $conn;
-//     }
-
-// }
+?>
